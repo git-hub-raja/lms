88f3f29e646953eed437b98d15775bc24762f572
RAJA <raja.hn91@yahoo.com>
2019-12-21 22:44:40 +0530

    Login changes updated.

---
+++ js/data.js
@@ -0,0 +1,12 @@
+var users = [
+    {
+        "user-type": "ADMIN",
+        "user-name": "Admin",
+        "password": "Admin123"
+    },
+    {
+        "user-type": "STUDENT",
+        "user-name": "StudentOne",
+        "password": "Student123"
+    }
+];
--- js/login.js
+++ js/login.js
@@ -1,7 +1,33 @@
 
 function logIn(){
-    document.getElementsByClassName("login-container")[0].style.display = "none";
-    document.getElementsByClassName("overlay")[0].style.display = "none";
+    var user_type = document.getElementById("user-type").value;
+    var user_name = document.getElementById("user-name").value;
+    var password = document.getElementById("password").value;
+    if(user_type == "" || user_type == undefined){
+        alert("Please select the USER TYPE.");
+        return false;
+    }else if(user_name == "" || user_name == undefined){
+        alert("Please select the USER NAME.");
+        return false;
+    }else if(password == "" || password == undefined){
+        alert("Please select the PASSWORD.");
+        return false;
+    }else{
+        var user_exists = users.filter(user => {
+            return user["user-type"] === user_type.trim()
+                && user["user-name"] === user_name.trim()
+                && user.password === password.trim();
+        })
+        if(user_exists.length > 0){
+            sessionStorage.setItem("user-type", user_type.trim());
+            sessionStorage.setItem("user-name", user_name.trim());
+            document.getElementsByClassName("login-container")[0].style.display = "none";
+            document.getElementsByClassName("overlay")[0].style.display = "none";
+        }else{
+            alert("Invalid USER NAME or PASSWORD.");
+            return false;
+        }
+    }
 }
 
 function close(){
@@ -9,7 +35,9 @@ function close(){
     document.getElementsByClassName("overlay")[0].style.display = "none";
 }
 
-function logout(){
+function logout(){    
+    sessionStorage.removeItem("user-type");
+    sessionStorage.removeItem("user-name");
     document.getElementsByClassName("login-container")[0].style.display = "block";
     document.getElementsByClassName("overlay")[0].style.display = "block";
 }
